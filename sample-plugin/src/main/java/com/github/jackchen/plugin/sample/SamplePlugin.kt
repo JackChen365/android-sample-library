9cd24a59f229817d2ce896cc89c948998e4c0bb0
Airsaid <airsaid1024@gmail.com>
2022-07-23 17:59:00 +0800

    Fix execution optimizations have been disabled of transformDebugClassesWithAsm

--- sample-plugin/src/main/java/com/github/jackchen/plugin/sample/SamplePlugin.kt
+++ sample-plugin/src/main/java/com/github/jackchen/plugin/sample/SamplePlugin.kt
@@ -11,78 +11,84 @@ import com.github.jackchen.plugin.sample.instrumentation.AndroidSampleTemplateCr
 import com.github.jackchen.plugin.sample.instrumentation.SampleAsmClassVisitorFactory
 import com.github.jackchen.plugin.sample.instrumentation.SampleClassHandler
 import com.github.jackchen.plugin.sample.task.MergeSourceFileAndDocTask
+import com.github.jackchen.plugin.sample.util.isAndroidProject
 import com.google.gson.Gson
-import org.gradle.api.Plugin
-import org.gradle.api.Project
+import org.gradle.api.*
 import org.gradle.configurationcache.extensions.capitalized
 import java.io.File
 
+/**
+ * @author JackChen
+ */
 class SamplePlugin : Plugin<Project> {
-    private fun isAndroidProject(project: Project): Boolean {
-        return null != project.plugins.findPlugin("com.android.application")
-    }
 
-    override fun apply(project: Project) {
-        if (!isAndroidProject(project)) return
-        configureTransformClass(project)
-        configureCollectSourceFileAndDocTask(project)
+  override fun apply(project: Project) {
+    if (!project.isAndroidProject()) {
+      throw GradleException("${project.name} is not an Android project.")
     }
+    configureTransformClass(project)
+    configureCollectSourceFileAndDocTask(project)
+  }
 
-    private fun configureCollectSourceFileAndDocTask(project: Project) {
-        project.afterEvaluate {
-            val appExtension = project.extensions.getByType(AppExtension::class.java)
-            appExtension.applicationVariants.forEach { applicationVariant ->
-                val task = project.tasks.create(
-                    MergeSourceFileAndDocTask.TASK_NAME + "With" +
-                            applicationVariant.flavorName.capitalized() +
-                            applicationVariant.buildType.name,
-                    MergeSourceFileAndDocTask::class.java
-                )
-                if (applicationVariant.mergeAssetsProvider.isPresent) {
-                    val mergeAssetsTask = applicationVariant.mergeAssetsProvider.get()
-                    mergeAssetsTask.dependsOn(task)
-                }
-            }
+  private fun configureTransformClass(project: Project) {
+    val androidComponentsExtension =
+      project.extensions.getByType(AndroidComponentsExtension::class.java)
+    androidComponentsExtension.onVariants { variant ->
+      // Clear the data every time when we register the transform.
+      SampleClassHandler.clearData()
+      variant.transformClassesWith(
+        SampleAsmClassVisitorFactory::class.java,
+        InstrumentationScope.PROJECT
+      ) {}
+      variant.setAsmFramesComputationMode(
+        FramesComputationMode.COMPUTE_FRAMES_FOR_INSTRUMENTED_METHODS
+      )
+    }
+    createSampleConfigurationClassAfterTransform(project)
+  }
+
+  private fun configureCollectSourceFileAndDocTask(project: Project) {
+    project.afterEvaluate {
+      val appExtension = project.extensions.getByType(AppExtension::class.java)
+      appExtension.applicationVariants.forEach { applicationVariant ->
+        val task = project.tasks.create(
+          MergeSourceFileAndDocTask.TASK_NAME + "With" +
+              applicationVariant.flavorName.capitalized() +
+              applicationVariant.buildType.name.capitalized(),
+          MergeSourceFileAndDocTask::class.java
+        )
+        if (applicationVariant.mergeAssetsProvider.isPresent) {
+          val mergeAssetsTask = applicationVariant.mergeAssetsProvider.get()
+          mergeAssetsTask.dependsOn(task)
         }
+      }
     }
+  }
 
-    private fun configureTransformClass(project: Project) {
-        val androidComponentsExtension = project.extensions.getByType(AndroidComponentsExtension::class.java)
-        androidComponentsExtension.onVariants { variant ->
-            // Clear the data every time when we register the transform.
-            SampleClassHandler.clearData()
-            variant.transformClassesWith(
-                SampleAsmClassVisitorFactory::class.java,
-                InstrumentationScope.PROJECT
-            ) { }
-            variant.setAsmFramesComputationMode(
-                FramesComputationMode.COMPUTE_FRAMES_FOR_INSTRUMENTED_METHODS
-            )
+  private fun createSampleConfigurationClassAfterTransform(project: Project) {
+    project.tasks.withType(TransformClassesWithAsmTask::class.java) { transformTask ->
+      transformTask.doLast(object : Action<Task> {
+        override fun execute(t: Task) {
+          val files = transformTask.outputs.files.files
+          val sampleList = SampleClassHandler.getSampleList()
+          val extensionList = SampleClassHandler.getExtensionList()
+          if (files.isNotEmpty()) {
+            val destFolder = files.first()
+            createSampleConfigurationClass(destFolder, sampleList, extensionList)
+          }
+          PathNodePrinter.printPathTree(sampleList)
         }
-        createSampleConfigurationClassAfterTransform(project)
+      })
     }
+  }
 
-    private fun createSampleConfigurationClassAfterTransform(project: Project) {
-        project.tasks.withType(TransformClassesWithAsmTask::class.java) { transformClassesTask ->
-            transformClassesTask.doLast {
-                val files = transformClassesTask.outputs.files.files
-                val sampleList = SampleClassHandler.getSampleList()
-                val extensionList = SampleClassHandler.getExtensionList()
-                if (files.isNotEmpty()) {
-                    val destFolder = files.first()
-                    createSampleConfigurationClass(destFolder, sampleList, extensionList)
-                }
-                PathNodePrinter.printPathTree(sampleList)
-            }
-        }
-    }
-
-    private fun createSampleConfigurationClass(
-        destFolder: File,
-        sampleItemList: MutableList<SampleItem>,
-        extensionList: MutableList<ExtensionItem>
-    ) {
-        val configurationJsonText = Gson().toJson(mapOf("samples" to sampleItemList, "extensions" to extensionList))
-        AndroidSampleTemplateCreator.create(destFolder, configurationJsonText)
-    }
+  private fun createSampleConfigurationClass(
+    destFolder: File,
+    sampleItemList: MutableList<SampleItem>,
+    extensionList: MutableList<ExtensionItem>
+  ) {
+    val configurationJsonText =
+      Gson().toJson(mapOf("samples" to sampleItemList, "extensions" to extensionList))
+    AndroidSampleTemplateCreator.create(destFolder, configurationJsonText)
+  }
 }
