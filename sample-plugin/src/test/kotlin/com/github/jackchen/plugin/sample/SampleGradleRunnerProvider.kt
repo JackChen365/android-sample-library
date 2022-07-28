0420bf08a975e342020681f96c88f7edbdc9529d
JackChen365 <>
2022-07-28 22:55:08 +0800

    Add a test case for incremental test

---
+++ sample-plugin/src/test/kotlin/com/github/jackchen/plugin/sample/SampleGradleRunnerProvider.kt
@@ -0,0 +1,18 @@
+package com.github.jackchen.plugin.sample
+
+import com.github.jackchen.gradle.test.toolkit.runner.GradleRunnerProvider
+import org.gradle.testkit.runner.GradleRunner
+import java.io.File
+
+class SampleGradleRunnerProvider : GradleRunnerProvider {
+    companion object {
+        private val sharedTestKitDir = File(".").resolve(".gradle").absoluteFile.also {
+            if (!it.exists()) it.mkdir()
+        }
+    }
+
+    override fun getGradleRunner(projectDir: File, gradleVersion: String): GradleRunner {
+        return GradleRunner.create().withProjectDir(projectDir).withGradleVersion(gradleVersion)
+            .withTestKitDir(sharedTestKitDir).forwardOutput()
+    }
+}
